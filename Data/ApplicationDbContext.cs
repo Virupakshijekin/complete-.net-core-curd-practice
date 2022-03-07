80e271c1d52492ea3991c87edd727f4cc80738bf
unknown <ramesh.p@MICROLABS.COM>
2022-03-07 11:48:26 +0530

    1st commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudCore.Data
+{
+    public class ApplicationDbContext
+    {
+    }
+}
